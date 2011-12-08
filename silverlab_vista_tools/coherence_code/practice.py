23ef1bb0cf84ead1922395f7ff3f366f220130e2
Alina Liberman <alinalib@gmail.com>
2011-12-07 18:45:29 -0800

    Adding coherence code

--- silverlab_vista_tools/coherence_code/practice.py
+++ silverlab_vista_tools/coherence_code/practice.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
 
     base_path = '/Volumes/Plata1/DorsalVentral/' # Change this to your path
     fmri_path = base_path + 'fmri/'
-    this_s='CG011611'
+    this_s='CG020611'
     TR = 2
     # The pass band is f_lb <-> f_ub.
     # Also, see: http://imaging.mrc-cbu.cam.ac.uk/imaging/DesignEfficiency
@@ -37,11 +37,24 @@ if __name__ == "__main__":
         # len(subjects[subject])= number of session per subject
         # len(subjects[subject][0][1])= number of different types of runs 
         # len(subjects[subject][1][1]['fix_nii'])= number of nifti files for that session
- # Just do the placebo session, 0=donepazil, 1=placebo
+        # Just do the placebo session, 0=donepazil, 1=placebo
         sess = subjects[subject][1]
         # Loop over sessions (donepezil/placebo):
+        roi_names=['R_V1', 'R_V2V', 'R_V3V', 'R_V2D', 'R_V3D','R_V4','R_V7', 'R_IPS1', 'R_IPS2', 'L_V1', 'L_V2',  'L_V3',  'L_V4', ]
+        roi_names=np.array(roi_names)
         ROI_files=[fmri_path+sess[0]+'/Inplane/ROIs/R_V1.mat',
-                   fmri_path+sess[0]+'/Inplane/ROIs/L_V1.mat']
+                   fmri_path+sess[0]+'/Inplane/ROIs/R_V2V.mat',
+                   fmri_path+sess[0]+'/Inplane/ROIs/R_V3V.mat',
+                   fmri_path+sess[0]+'/Inplane/ROIs/R_V2D.mat',
+                   fmri_path+sess[0]+'/Inplane/ROIs/R_V3D.mat',
+                   fmri_path+sess[0]+'/Inplane/ROIs/R_V4.mat',
+                   fmri_path+sess[0]+'/Inplane/ROIs/R_V7.mat',
+                   fmri_path+sess[0]+'/Inplane/ROIs/R_IPS1.mat',
+                   fmri_path+sess[0]+'/Inplane/ROIs/R_IPS2.mat',
+                   fmri_path+sess[0]+'/Inplane/ROIs/L_V1.mat',
+                   fmri_path+sess[0]+'/Inplane/ROIs/L_V2.mat',
+                   fmri_path+sess[0]+'/Inplane/ROIs/L_V3.mat',
+                   fmri_path+sess[0]+'/Inplane/ROIs/L_V4.mat']
 
         nifti_path = fmri_path +sess[0] + '/%s_nifti/' % sess[0]
 
@@ -56,22 +69,17 @@ if __name__ == "__main__":
         t_right = []
 
         # len(t_fix)= number of ROIs
-         for this_fix in sess[1]['fix_nii']:
+        for this_fix in sess[1]['fix_nii']:
             t_fix.append(load_nii(nifti_path+this_fix, ROI_coords,TR,
                                     normalize='percent', average=True, verbose=True))
         # Change to an array (numSess, numROIs, numTime points)
         t_fixArray=np.array(t_fix)
         # Swap ROI and session dimension
         t_fixArray=np.transpose(t_fixArray, (1,0,2))
+        shape=t_fixArray.shape
+        allROIs=np.reshape(t_fixArray, [shape[0], shape[1]*shape[2]])
+        fixTS=ts.TimeSeries(allROIs, sampling_interval=TR)
 
-        roi1=t_fixArray[0]
-        roi2=t_fixArray[1]
-
-        roi1All=[]
-        for numROIs in roi1:
-            roi1All+=numROIs
-
-        roi1list=roi1.tolist()    
-        roiAll=roi1list[0]+roi1list[1]+roi1list[2]    
-    
-            
+        C=CorrelationAnalyzer(fixTS)
+        fig01 = drawmatrix_channels(C.corrcoef, roi_names, size=[10., 10.], color_anchor=0)
+      
