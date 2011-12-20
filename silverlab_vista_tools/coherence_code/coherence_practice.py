5df0c3a70530598f0ca6110200933b113f109e48
Alina Liberman <alinalib@gmail.com>
2011-12-20 11:07:45 -0800

    update

---
+++ silverlab_vista_tools/coherence_code/coherence_practice.py
@@ -0,0 +1,106 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+
+import vista_utils as tsv # Get it at: https://github.com/arokem/vista_utils
+from nitime.fmri.io import time_series_from_file as load_nii 
+import nitime.timeseries as ts
+import nitime.viz as viz
+
+from nitime.analysis import CorrelationAnalyzer, CoherenceAnalyzer
+#Import utility functions:
+from nitime.utils import percent_change
+from nitime.viz import drawmatrix_channels, drawgraph_channels
+from nitime.viz import drawmatrix_channels, drawgraph_channels, plot_xcorr
+
+
+import subjects
+reload(subjects) # In case you make changes in there while you analyze
+from subjects import subjects, rois
+
+
+
+if __name__ == "__main__":
+
+    base_path = '/Volumes/Plata1/DorsalVentral/' # Change this to your path
+    fmri_path = base_path + 'fmri/'
+    session=0 # 0= donepazil, 1=placebo
+    TR = 2
+    # The pass band is f_lb <-> f_ub.
+    # Also, see: http://imaging.mrc-cbu.cam.ac.uk/imaging/DesignEfficiency
+    f_ub = 0.15
+    f_lb = 0.01
+    
+    # The upsample factor between the Inplane and the Gray:
+    # Inplane Voxels: .867 x .867 x 3.3, Functional voxels: 3 x 3 x 3.3
+    up_samp = [3.4595,3.4595,1.0000]
+
+    for subject in subjects:
+        # len(subjects[subject])= number of session per subject
+        # len(subjects[subject][0][1])= number of different types of runs 
+        # len(subjects[subject][1][1]['fix_nii'])= number of nifti files for that session
+
+        sess = subjects[subject][session]
+        # Get ROIs
+        roi_names=np.array(rois)
+        ROI_files=[]
+        for roi in rois:
+            ROI_files.append(fmri_path+sess[0]+'/Inplane/ROIs/' +roi +'.mat')
+            
+        nifti_path = fmri_path +sess[0] + '/%s_nifti/' % sess[0]
+
+        # Get the coordinates of the ROIs, while accounting for the
+        # up-sampling:
+        # 
+        ROI_coords = [tsv.upsample_coords(tsv.getROIcoords(f),up_samp)
+                           for f in ROI_files]
+         # Initialize lists for each behavioral condition:
+        t_fix = []
+        t_left = []
+        t_right = []
+
+        # len(t_fix)= number of ROIs
+        for this_fix in sess[1]['fix_nii']:
+            t_fix.append(load_nii(nifti_path+this_fix, ROI_coords,TR,
+                                    normalize='percent', average=True, verbose=True))
+        # Change to an array (numSess, numROIs, numTime points)
+        t_fixArray=np.array(t_fix)
+        # Swap ROI and session dimension
+        t_fixArray=np.transpose(t_fixArray, (1,0,2))
+        shape=t_fixArray.shape
+        allROIs=np.reshape(t_fixArray, [shape[0], shape[1]*shape[2]])
+        fixTS=ts.TimeSeries(allROIs, sampling_interval=TR)
+
+        # Get roi correlations
+        C=CorrelationAnalyzer(fixTS)
+        fig01 = drawmatrix_channels(C.corrcoef, roi_names, size=[10., 10.], color_anchor=0)
+        
+        # Get cross correlations
+        xc = C.xcorr_norm
+        idx_rv1 = np.where(roi_names == 'R_V1')[0]
+        idx_rv2v = np.where(roi_names == 'R_V2V')[0]
+        idx_rv3v = np.where(roi_names == 'R_V3V')[0]
+        idx_rv4 = np.where(roi_names == 'R_V4')[0]
+        idx_rv2d= np.where(roi_names == 'R_V2D')[0]
+        idx_rv3d = np.where(roi_names == 'R_V3D')[0]
+
+        fig02 = plot_xcorr(xc,((idx_rv1, idx_rv2v),(idx_rv1, idx_rv3v)),line_labels=['rV2V', 'rV3V'])
+
+        # Get coherence
+        Coh = CoherenceAnalyzer(fixTS)
+
+        # Get the index for the frequencies inside the ub and lb
+        freq_idx = np.where((Coh.frequencies > f_lb) * (Coh.frequencies < f_ub))[0]
+
+        # Extract coherence
+        coher = np.mean(Coh.coherence[:, :, freq_idx], -1)  # Averaging on the last dimension
+        fig03 = drawmatrix_channels(coher, roi_names, size=[10., 10.], color_anchor=0)
+
+        # Focus on areas of interest
+        idx = np.hstack([idx_rv1, idx_rv2v, idx_rv3v, idx_rv4, idx_rv2d, idx_rv3d ])
+        idx1 = np.vstack([[idx[i]] * 6 for i in range(6)]).ravel()
+        idx2 = np.hstack(6 * [idx])
+        coher_specific = Coh.coherence[idx1, idx2].reshape(6, 6, Coh.frequencies.shape[0])
+        coherence = np.mean(coher_specific[:, :, freq_idx], -1)  # Averaging on the last dimension
+        fig04 = drawgraph_channels(coherence, roi_names[idx]) # Draw network
+                                                      
