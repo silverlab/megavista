4ea0b485fe3e1489325cd0c56ddbede687bbafa0
Alina Liberman <alinalib@gmail.com>
2012-01-31 14:38:28 -0800

    changed rd_mr files for analyzing CG_20120130

--- silverlab_vista_tools/Rachel/LGN_Code/rd_mrMakeDesign_MPLocalizerColor.m
+++ silverlab_vista_tools/Rachel/LGN_Code/rd_mrMakeDesign_MPLocalizerColor.m
@@ -5,11 +5,12 @@
 % cd /Volumes/LaCie/fMRI/Expt_Files/KS_20111212/MagnoParvoLocalizer_Color_20111211_7T/
 % cd /Volumes/LaCie/fMRI/Expt_Files/AV_20111213/MagnoParvoLocalizer_Color_20111211_7T/
 % cd /Volumes/Plata1/LGN/Expt_Files/KS_20111214/MagnoParvoLocalizer_Color_20111211_7T/
-cd /Volumes/Plata1/LGN/Expt_Files/RD_20111214/MagnoParvoLocalizer_Color_20111211_7T/
+% cd /Volumes/Plata1/LGN/Expt_Files/RD_20111214/MagnoParvoLocalizer_Color_20111211_7T/
+cd /Volumes/Plata1/LGN/Expt_Files/CG_20120130/MagnoParvoLocalizer_Color_20111116/
 
-subjectID = 'RD';
-runs = 1:12;
-scanDate = '20111214';
+subjectID = 'CG';
+runs = 1:14;
+scanDate = '20120130';
 
 includeResponseCond = 0;
 includeColors = 1;
--- silverlab_vista_tools/Rachel/LGN_Code/rd_mrMakeMrInit2Params.m
+++ silverlab_vista_tools/Rachel/LGN_Code/rd_mrMakeMrInit2Params.m
@@ -13,8 +13,8 @@ function params = rd_mrMakeMrInit2Params
 % ------------------------------------------------------------------------
 % Here we have the most common analysis settings that are specific to an
 % individual experiment
-subjectID = 'RD';
-description = 'RD_20111214 MPLocalizerColor_7T';
+subjectID = 'CG';
+description = 'CG_20120130 MPLocalizerColor_3T';
 comments = '';
 
 % Scan groups
@@ -22,12 +22,13 @@ comments = '';
 % scanGroups = {[1 6], 2:5};
 % scanGroups = {[1 10], 2:9};
 % scanGroups = {[1 9], 2:8};
-scanGroups = {[1 14], 2:13};
+scanGroups = {[1 16], 2:15};
+% scanGroups = {[1 14], 2:13};
 % scanGroups = {[1 11], 2:10}; % scan numbers in each scan group
 % Keep frames
 % scanGroupKeepFrames = {[4 135]};
-scanGroupKeepFrames = {[16 -1], [4 135]};
-% scanGroupKeepFrames = {[6 132], [4 -1]}; % [frames-to-discard frames-to-keep]
+% scanGroupKeepFrames = {[16 -1], [4 135]}; % 7T
+scanGroupKeepFrames = {[6 132], [4 -1]}; % 3T [frames-to-discard frames-to-keep]
 % Annotations
 scanGroupNames = {'hemi','mp'};
 
@@ -38,8 +39,8 @@ scansWithParfile = scanGroups{2};
 % Coherence analysis
 % coherenceScanGroups = 0;
 coherenceScanGroups = 1;
-nCycles = 8;
-% nCycles = 11;
+% nCycles = 8; % 7T
+nCycles = 11; % 3T
 
 % GLM analysis
 % glmScanGroups = 1;
