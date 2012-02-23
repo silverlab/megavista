b31353d87fceec112c1d018fd89ebaf653ac59dd
Rachel Denison <rdenison@berkeley.edu>
2012-02-23 13:53:56 -0800

    added tsdiffana folder including rd_tsdiff functions

---
+++ users/Rachel/tsdiffana/rd_tsdiffana.m
@@ -0,0 +1,55 @@
+function [td, globals, slicediff, imgs] = rd_tsdiffana(imgs, vf, fg, pflags)
+% wrapper and plotter for timediff function
+% 
+% imgs    - string list of images
+% vf      - non zero if difference images required
+% fg      - figure to plot results (spm figure default)
+% pflags  - plot flags - 0 or more of 'r'  - plot realignment params
+%
+% Modified by RD:
+% The main difference between this and tsdiffana is that imgs can be a
+% cell array of string file names. Also calls rd_tsdiffplot instead of
+% tsdiffplot.
+
+[td, globals, slicediff] = deal([]);
+if nargin < 1
+  imgs = [];
+end
+if isempty(imgs)  
+  imgs = cbu_get_imgs(Inf, 'Select time series images');
+end
+if isempty(imgs), return, end
+if nargin < 2
+  vf =  0;
+end
+noplot = 0;
+if nargin < 3
+  fg = [];
+elseif empty(fg)
+  noplot = 1;
+end
+if nargin < 4
+  pflags = '';
+end
+
+if iscell(imgs)
+    [p f e] = fileparts(imgs{1});
+else
+    [p f e] = fileparts(imgs(1,:));
+end
+
+if vf 
+  flags = 'mv';
+else
+  flags = 'm';
+end
+
+[td globals slicediff] = timediff(imgs,flags);
+tdfn = fullfile(p,'timediff.mat');
+save(tdfn, 'imgs', 'td', 'globals', 'slicediff');
+
+if ~noplot
+  rd_tsdiffplot(tdfn,fg, pflags);
+end
+return
+
