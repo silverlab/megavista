2cabebe34a20c4ef85c2f43fb958dca58e5affe5
Rachel Denison <rdenison@berkeley.edu>
2012-02-23 13:38:05 -0800

    completed most of rd_qualityControl

--- users/Rachel/LGN_Code/rd_qualityControl.m
+++ users/Rachel/LGN_Code/rd_qualityControl.m
@@ -2,6 +2,9 @@
 
 % FIRST go to _nifni folder
 
+% get current path info
+[p f ext] = fileparts(pwd);
+
 % organize files, unzip niftis
 !mkdir QualityControl
 !cp epi*hemi.nii.gz QualityControl
@@ -17,7 +20,19 @@ end
 
 % tsdiffana
 % tsdiffana('epi01_hemi_mcf.nii')
-rd_tsdiffana(epis)
+rd_tsdiffana(epis,[],100) % let the figure handle be 100
+
+% add session name to figure
+pos = get(gca,'Position');
+text(0.5, pos(2)/2.5, f ,'HorizontalAlignment','center');
+
+%save the output figure
+saveas(100,'tsdiffana_output.pdf')
 
 % make mean image
-spm_imcalc_ui('epi01_hemi_mcf.nii', 'myseries_mean.nii', 'mean(X)', {1;0;4;0})
+% spm_imcalc_ui('epi01_hemi_mcf.nii', 'myseries_mean.nii', 'mean(X)', {1;0;4;0})
+spm_imcalc_ui(epis, 'meanepi.nii', 'mean(X)', {1;0;4;0})
+
+% move QualityControl folder into session directory
+cd ..
+movefile('QualityControl', p)
