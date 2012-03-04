ef57d716796391318f4a9f262c6d538f621be88e
Rachel Denison <rdenison@berkeley.edu>
2012-03-04 14:00:09 -0800

    added files for working with data from multiple subjects, as in a group analysis

---
+++ users/Rachel/LGN_Code/rd_getAnalysisFilePath.m
@@ -0,0 +1,21 @@
+function filePath = rd_getAnalysisFilePath(subjectDirs, scanner, subject, hemi, analysisExtension)
+%
+% function filePath = rd_getAnalysisFilePath(subjectDirs, scanner, subject, hemi, analysisExtension)
+%
+% gets the filepath to a specific analysis file for any subject and
+% hemisphere.
+
+subjectDir{1} = subjectDirs{subject,1};
+subjectDir{2} = subjectDirs{subject,2};
+subjectDir{3} = subjectDirs{subject,3};
+
+fileDirectory = sprintf('/Volumes/Plata1/LGN/Scans/%s/%s/%s/ROIAnalysis/%s',...
+    scanner, subjectDir{1}, subjectDir{2}, subjectDir{3});
+
+fileBase = sprintf('lgnROI%d', hemi);
+
+dataDir = dir(sprintf('%s/%s_%s.mat', ...
+    fileDirectory, fileBase, analysisExtension));
+fileName = dataDir.name;
+
+filePath = sprintf('%s/%s', fileDirectory, fileName);
---
+++ users/Rachel/LGN_Code/rd_lgnSubjects.m
@@ -0,0 +1,16 @@
+function [subjectDirs3T subjectDirs7T] = rd_lgnSubjects
+%
+% [subjectDirs3T subjectDirs7T] = rd_lgnSubjects
+%
+% gets subject directory info for all the subjects
+
+subjectDirs3T = {'AV_20111117_session', 'AV_20111117_n', 'ROIX01';
+                'AV_20111128_session', 'AV_20111128_n', 'ROIX01/Runs1-9';
+                'CG_20120130_session', 'CG_20120130_n_LOW', 'ROIX01';
+                'CG_20120130_session', 'CG_20120130_n_HIGH', 'ROIX01';
+                'RD_20120205_session', 'RD_20120205_n', 'ROIX01'};
+            
+subjectDirs7T = {'KS_20111212_Session', 'KS_20111212_15mm', 'ROIX01';
+                'AV_20111213_Session', 'AV_20111213', 'ROIX01';
+                'KS_20111214_Session', 'KS_20111214', 'ROIX01';
+                'RD_20111214_Session', 'RD_20111214', 'ROIX01'};
