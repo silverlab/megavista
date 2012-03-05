beaeae9799da0ca6fb803e0ad34f29e79db6ee95
Rachel Denison <rdenison@berkeley.edu>
2012-03-04 16:01:20 -0800

    added script to run a given individual analysis script on multiple subjects

---
+++ users/Rachel/LGN_Code/rd_runIndivAnalysis.m
@@ -0,0 +1,27 @@
+% rd_runIndivAnalysis.m
+
+scanner = '7T';
+
+[subjectDirs3T subjectDirs7T] = rd_lgnSubjects;
+switch scanner
+    case '3T'
+        subjectDirs = subjectDirs3T;
+    case '7T'
+        subjectDirs = subjectDirs7T;
+end
+
+subjects = 1:size(subjectDirs,1);
+nSubjects = numel(subjects);
+
+% run specified individual analysis script in subject directory
+for iSubject = 1:nSubjects
+    subject = subjects(iSubject);
+    
+    [fpath fdir] = rd_getAnalysisFilePath(subjectDirs, scanner, subject);
+    
+    % go to subject directory
+    cd(fdir)
+    
+    % run script
+    rd_centerOfMass_multiVoxData
+end
