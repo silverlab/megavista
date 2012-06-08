028c14f275202037a3a0d04ca710d8f111578ee5
Alina Liberman <alinalib@gmail.com>
2012-06-08 16:12:39 -0700

    updated rd_voxTimeCourse.m to plot labeled figs, save figs, save analysis

--- users/Rachel/LGN_Code/rd_voxTimeCourse.m
+++ users/Rachel/LGN_Code/rd_voxTimeCourse.m
@@ -3,15 +3,20 @@
 % see tc_init.m
 
 %% setup
-hemi = 2;
+hemi = 1;
 voxelSelectionOption = 'varExp';
 varThresh = 0;
 betaThresh = 0;
+
+saveData = 1;
+saveFigs = 1;
 
 %% file I/O
 fileBase = sprintf('lgnROI%d', hemi);
 analysisExtension = '_multiVoxFigData';
 loadPath = sprintf('%s%s.mat', fileBase, analysisExtension);
+analysisSavePath = sprintf('%s_timeCourses_%s.mat', fileBase, datestr(now,'yyyymmdd'));
+plotSavePath = sprintf('figures/%sPlot_timeCourses', fileBase);
 
 %% load data
 load(loadPath)
@@ -24,6 +29,13 @@ params = figData.params;
 
 % reset any params
 params.normBsl = 1;
+
+% calculate frame window (in TRs) used by er_chopTSeries2 (lines 165-171)
+timeWindow = params.timeWindow;
+TR = figData.trials.TR;
+t1 = min(timeWindow);  t2 = max(timeWindow);
+f1 = fix(t1 / TR);  f2 = fix(t2 / TR);
+frameWindow = f1:f2;
 
 %% voxel selection
 switch voxelSelectionOption
@@ -52,33 +64,52 @@ for iVox = 1:nVox
     
     voxMeanTcs(:,:,iVox) = voxtc.meanTcs;
 end
+voxMeanTcs_dimHeaders = {'TR','cond','vox'};
 
 %% plot mean tcs for all voxels
 for iCond = 1:nConds
-    figure
-%     plot(voxtc.timeWindow, squeeze(voxMeanTcs(:,iCond,:)));
-    plot(squeeze(voxMeanTcs(:,iCond,:)));
-    title(sprintf('%s, %s', condNames{iCond}, voxDescrip));
+    fig1(iCond) = figure;
+    plot(frameWindow*TR, squeeze(voxMeanTcs(:,iCond,:)));
+    xlabel('time (s)')
+    ylabel('BOLD amplitude')
+    title(sprintf('Hemi %d, %s, %s', hemi, condNames{iCond}, voxDescrip));
     
     hold on
-%     plot(voxtc.timeWindow, mean(squeeze(voxMeanTcs(:,iCond,:)),2),...
-%         'k','LineWidth',2)
-    plot(mean(squeeze(voxMeanTcs(:,iCond,:)),2),...
+    plot(frameWindow*TR, mean(squeeze(voxMeanTcs(:,iCond,:)),2),...
         'k','LineWidth',2)
+    plot(frameWindow*TR, zeros(size(frameWindow)),'--k','LineWidth',1)
 end
 
 %% plot difference tcs
 for iCond = 2:nConds
-    figure
+    fig2(iCond) = figure;
     condDiff = squeeze(voxMeanTcs(:,iCond,:)-voxMeanTcs(:,1,:));
-%     plot(voxtc.timeWindow, condDiff);
-    plot(condDiff);
-    title(sprintf('%s - %s, %s', ...
-        condNames{iCond}, condNames{1}, voxDescrip));
+    plot(frameWindow*TR, condDiff);
+    xlabel('time (s)')
+    ylabel('BOLD amplitude')
+    title(sprintf('Hemi %d, %s - %s, %s', ...
+        hemi, condNames{iCond}, condNames{1}, voxDescrip));
     
     hold on
-%     plot(voxtc.timeWindow, mean(squeeze(voxMeanTcs(:,iCond,:)-voxMeanTcs(:,1,:)),2),...
-%         'k','LineWidth',2)
-    plot(mean(squeeze(voxMeanTcs(:,iCond,:)-voxMeanTcs(:,1,:)),2),...
+    plot(frameWindow*TR, mean(squeeze(voxMeanTcs(:,iCond,:)-voxMeanTcs(:,1,:)),2),...
         'k','LineWidth',2)
+    plot(frameWindow*TR, zeros(size(frameWindow)),'--k','LineWidth',1)
 end
+
+%% save data
+if saveData
+    save(analysisSavePath, 'hemi', 'voxelSelectionOption', 'varThresh', 'betaThresh', ...
+        'frameWindow', 'TR', 'voxMeanTcs', 'voxMeanTcs_dimHeaders');
+end
+
+%% save figs
+if saveFigs
+    for iF = 1:numel(fig1)
+        print(fig1(iF),'-djpeg', sprintf('%s_%s_%s', plotSavePath, condNames{iF}, datestr(now,'yyyymmdd')));
+    end
+    
+    for iF = find(fig2)
+        print(fig2(iF),'-djpeg', sprintf('%s_%s-%s_%s', plotSavePath, condNames{iF}, condNames{1}, datestr(now,'yyyymmdd')));
+    end
+end
+
