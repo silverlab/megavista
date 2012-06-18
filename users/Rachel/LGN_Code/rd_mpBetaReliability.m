e9990dd0aa03a61b8d49170c0ce43fb33324608a
Rachel Denison <rdenison@berkeley.edu>
2012-06-17 22:06:50 -0700

    added correlation calculations to rd_mpBetaReliability

--- users/Rachel/LGN_Code/rd_mpBetaReliability.m
+++ users/Rachel/LGN_Code/rd_mpBetaReliability.m
@@ -1,6 +1,10 @@
 % rd_mpBetaReliability.m
+%
+% reliability of betaM,P,M-P, and classification across individual scans
 
+%% setup
 hemi = 1;
+correlationType = 'corrcoef'; % ['corrcoef','rankcorr']
 
 dataFile = dir(sprintf('lgnROI%d_indivScanData_multiVoxel*', hemi));
 if numel(dataFile)~=1
@@ -18,12 +22,51 @@ nConds = size(betas,2);
 
 betasMPDiff = squeeze(betas(:,1,:) - betas(:,2,:));
 
+betas(:,3,:) = betasMPDiff;
+
+%% rank order of beta values for plotting
+% get rank order for mean of betaM and betaP
 betasMean = squeeze(mean(betas,1));
 [betasMeanS, order] = sort(betasMean,2);
 
+% get rank order for mean of betaM-P
 betasMPDiffMean = mean(betasMPDiff,1);
 [betasMPDiffMeanS, mpDiffOrder] = sort(betasMPDiffMean,2);
 
+%% mean inter-run correlation (mean of pairwise correlations)
+for iCond = 1:nConds
+    switch correlationType
+        case 'corrcoef'
+            r = corrcoef(squeeze(betas(:,iCond,:))'); % [nRuns x nRuns] corrcoef
+        case 'rankcorr'
+            r = corr(squeeze(betas(:,iCond,:))'); 
+        otherwise
+            error('correlationType not found')
+    end
+
+    % symmetric matrix, so just take the lower triangular values
+    rvals = tril(r,-1); % shift by 1 to exclude main diagonal
+    
+    % remove matrix entries of zero and make 1D
+    rvals = rvals(:);
+    rvals(rvals==0) = [];
+    
+    runPairCorrs(:,:,iCond) = r;
+    runPairCorrVals(:,iCond) = rvals;
+end
+
+switch correlationType
+    case 'corrcoef'
+        r = corrcoef(squeeze(betas(:,iCond,:))'); % [nRuns x nRuns] corrcoef
+    case 'rankcorr'
+        r = corr(squeeze(betas(:,iCond,:))');
+    otherwise
+        error('correlationType not found')
+end
+
+
+%% figures
+% each run sorted by rank order of mean
 for iCond = 1:nConds
     figure
     subplot(1,2,1)
@@ -44,6 +87,7 @@ plot(betasMPDiff','.')
 axis square
 rd_supertitle('M-P')
 
+% pairwise correlations
 for iCond = 1:nConds
     figure
     plotmatrix(squeeze(betas(:,iCond,:))')
