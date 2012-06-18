c26ec37a7637b00bbdd6805bcd82662aa005d2db
Rachel Denison <rdenison@berkeley.edu>
2012-06-17 23:36:19 -0700

    updated rd_mpBetaReliability to treat betaM-P like an extra condition, compute pairwise correlations, and plot correlations

--- users/Rachel/LGN_Code/rd_mpBetaReliability.m
+++ users/Rachel/LGN_Code/rd_mpBetaReliability.m
@@ -12,34 +12,53 @@ if numel(dataFile)~=1
 end
 load(dataFile.name)
 
-condNames = {'M','P'};
+condNames = {'M','P','M-P'};
 nRuns = numel(uiData);
 
+%% get betas from each run
 for iRun = 1:nRuns
     betas(iRun,:,:) = uiData(iRun).mv.glm.betas(:,1:2,:);
 end
-nConds = size(betas,2);
 
-betasMPDiff = squeeze(betas(:,1,:) - betas(:,2,:));
+%% make betaVals to store original betas and all calculated beta vals
+betaVals = betas;
 
-betas(:,3,:) = betasMPDiff;
+% add betaM-P to betaVals as the third condition
+betaVals(:,end+1,:) = betas(:,1,:) - betas(:,2,:);
+nConds = size(betaVals,2);
 
 %% rank order of beta values for plotting
 % get rank order for mean of betaM and betaP
-betasMean = squeeze(mean(betas,1));
-[betasMeanS, order] = sort(betasMean,2);
+betaValsMean = squeeze(mean(betaVals,1));
+[betaValsMeanS, order] = sort(betaValsMean,2);
 
-% get rank order for mean of betaM-P
-betasMPDiffMean = mean(betasMPDiff,1);
-[betasMPDiffMeanS, mpDiffOrder] = sort(betasMPDiffMean,2);
+%% figures
+% each run sorted by rank order of mean
+for iCond = 1:nConds
+    figure
+    subplot(1,2,1)
+    plot(squeeze(betaVals(:,iCond,order(iCond,:)))','.')
+    axis square
+    subplot(1,2,2)
+    plot(squeeze(betaVals(:,iCond,:))','.')
+    axis square
+    rd_supertitle(condNames{iCond})
+end
+
+% pairwise correlations (plot matrix)
+for iCond = 1:nConds
+    figure
+    plotmatrix(squeeze(betaVals(:,iCond,:))')
+    rd_supertitle(condNames{iCond})
+end
 
 %% mean inter-run correlation (mean of pairwise correlations)
 for iCond = 1:nConds
     switch correlationType
         case 'corrcoef'
-            r = corrcoef(squeeze(betas(:,iCond,:))'); % [nRuns x nRuns] corrcoef
+            r = corrcoef(squeeze(betaVals(:,iCond,:))'); % [nRuns x nRuns] corrcoef
         case 'rankcorr'
-            r = corr(squeeze(betas(:,iCond,:))'); 
+            r = corr(squeeze(betaVals(:,iCond,:))'); 
         otherwise
             error('correlationType not found')
     end
@@ -55,46 +74,34 @@ for iCond = 1:nConds
     runPairCorrVals(:,iCond) = rvals;
 end
 
-switch correlationType
-    case 'corrcoef'
-        r = corrcoef(squeeze(betas(:,iCond,:))'); % [nRuns x nRuns] corrcoef
-    case 'rankcorr'
-        r = corr(squeeze(betas(:,iCond,:))');
-    otherwise
-        error('correlationType not found')
-end
-
+runPairCorrMeans = mean(runPairCorrVals);
 
 %% figures
-% each run sorted by rank order of mean
-for iCond = 1:nConds
-    figure
-    subplot(1,2,1)
-    plot(squeeze(betas(:,iCond,order(iCond,:)))','.')
-    axis square
-    subplot(1,2,2)
-    plot(squeeze(betas(:,iCond,:))','.')
-    axis square
-    rd_supertitle(condNames{iCond})
-end
-
-figure
-subplot(1,2,1)
-plot(betasMPDiff(:,mpDiffOrder)','.')
-axis square
-subplot(1,2,2)
-plot(betasMPDiff','.')
-axis square
-rd_supertitle('M-P')
-
 % pairwise correlations
+f = figure;
 for iCond = 1:nConds
-    figure
-    plotmatrix(squeeze(betas(:,iCond,:))')
-    rd_supertitle(condNames{iCond})
+    subplot(2,nConds,iCond)
+    imagesc(runPairCorrs(:,:,iCond),[-1 1]);
+    title(condNames{iCond})
+    xlabel('run')
+    if iCond==1
+        ylabel('run')
+    end
+    
+    subplot(2,nConds,iCond+nConds);
+    hist(runPairCorrVals(:,iCond))
+    xlim([-1 1])
+    ax = axis;
+    title(condNames{iCond})
+    xlabel('r')
+    text(ax(1)*0.9, ax(4)*0.9, sprintf('mean\n%.02f',runPairCorrMeans(iCond)))
+    if iCond==1
+        ylabel('num pairwise correlations')
+    end
 end
+rd_supertitle(sprintf('Hemi %d, run-to-run beta correlations',hemi));
 
-figure
-plotmatrix(betasMPDiff')
-rd_supertitle('M-P')
-
+% raise title off of subplot titles
+titlePos = get(gca,'Position');
+titlePos(2) = titlePos(2)*1.27;
+set(gca,'Position',titlePos);
