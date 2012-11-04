355ffceb334481eb3b142699482a2f92bfaedc7e
Rachel Denison <rdenison@berkeley.edu>
2012-11-04 15:05:33 -0800

    added script rd_nipy_connectivity_roi_to_roi to do seed -> target roi connectivity analysis with nifti

---
+++ users/Rachel/LGN_Code/python_analysis/rd_nipy_connectivity_roi_to_roi.py
@@ -0,0 +1,147 @@
+# rd_nipy_connectivity_roi_to_roi.py
+
+import os
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io as sio
+import scipy.stats as stats
+
+import nibabel as nib
+
+import nitime.analysis as nta
+import nitime.fmri.io as ntio
+
+import rd_nipy_visualize as visualize
+
+# file i/o
+session_dir = '/Volumes/Plata1/LGN/Scans/7T/JN_20120808_Session/JN_20120808_fslDC/'
+# roi_dir = os.path.join(session_dir, 'Masks/')
+roi_dir = '/Volumes/Plata1/motion_prf/subjects/JN_08092012_7T/Masks/'
+data_dir = os.path.join(session_dir, 'JN_20120808_fslDC_nifti/')
+out_dir = os.path.join(session_dir, 'ConnectivityAnalysis/')
+
+data_file = os.path.join(data_dir, 'epi01_fix_fsldc.nii.gz')
+
+# ROIs to use as seeds
+seed_rois = ['LLGN_ecc0','LLGN_ecc14','LLGN_polar5',
+    'RLGN_ecc2','RLGN_ecc9','RLGN_polar4']
+
+# ROIs to use as targets
+target_rois = ['LV1_ecc0-2', 'LV1_ecc10-18', 'LV1_polar602-026', 'LV1_polar474-526',
+     'RV1_ecc1-3', 'RV1_ecc7-11', 'RV1_polar174-226', 'RV1_polar374-426']
+
+# define TR and frequency band of interest
+#roi_name = 'V3'
+TR = 2
+f_lb = 0.01
+f_ub = 0.15
+
+# save results?
+save_nii = 1
+save_fig = 1
+
+# load data
+data = nib.load(data_file)
+volume_shape = data.shape[:-1]
+
+# Get seed data
+# initialize seed time series
+seed_ts = []
+
+for seed_name in seed_rois:
+    print '\n', seed_name
+    seed_file = os.path.join(roi_dir, '{}.nii.gz'.format(seed_name))
+
+    # load roi mask
+    seed_mask = nib.load(seed_file)
+    
+    # find the coordinates of the seed voxels
+    seed_vals = seed_mask.get_data()
+    seed_coords = np.array(np.where(seed_vals==1))
+
+    # make the seed time series (mean of roi time series)
+    seed_ts.append(ntio.time_series_from_file(data_file,
+                        seed_coords,
+                        TR=TR,
+                        normalize=None,
+                        average=True,
+                        filter=dict(lb=f_lb,
+                            ub=f_ub,
+                            method='boxcar'),
+                        verbose=True))
+
+# Get target data
+# initialize target time series
+target_ts = []
+
+for target_name in target_rois:
+    print '\n', target_name
+    target_file = os.path.join(roi_dir, '{}.nii.gz'.format(target_name))
+
+    # load roi mask
+    target_mask = nib.load(target_file)
+
+    # find the coordinates of cortex voxels
+    target_vals = target_mask.get_data()
+    target_coords = np.array(np.where(target_vals==1))
+
+    # make the target time series
+    target_ts.append(ntio.time_series_from_file(data_file,
+                        target_coords,
+                        TR=TR,
+                        normalize=None,
+                        filter=dict(lb=f_lb,
+                            ub=f_ub,
+                            method='boxcar'),
+                        verbose=True))
+
+# correlation analyzer
+corA = nta.SeedCorrelationAnalyzer(seed_ts, target_ts)
+
+# coherence analyzer
+cohA = nta.SeedCoherenceAnalyzer(seed_ts, target_ts,
+            method=dict(NFFT=20))
+
+# select frequency band
+freq_idx = np.where((cohA.frequencies > f_lb) * (cohA.frequencies < f_ub))[0]
+
+# extract correlation and coherence values
+print 'Calculating correlation and coherence'
+coh = np.mean(cohA.coherence[:, freq_idx], -1)
+cor = corA.corrcoef
+
+# make coh and cor images
+coords_indices = list(target_coords)
+
+coh_im = np.zeros(volume_shape)
+cor_im = np.zeros(volume_shape)
+
+coh_im[coords_indices] = coh
+cor_im[coords_indices] = cor
+   
+# save the images as niftis
+if save_nii:
+	print 'Saving niftis'
+	coh_nii = nib.Nifti1Image(coh_im, data.get_affine())
+	cor_nii = nib.Nifti1Image(cor_im, data.get_affine())
+
+	coh_nii.to_filename(coh_nii_file)
+	cor_nii.to_filename(cor_nii_file)
+
+# display the coh and coh maps
+fig_coh = visualize.display_slices(coh_im, 0, 1)
+fig_coh.suptitle('coherence, {0} seed'.format(seed_name))
+plt.show()
+
+fig_cor = visualize.display_slices(cor_im, 0, 1)
+fig_cor.suptitle('correlation {0} seed'.format(seed_name))
+plt.show()
+
+# save the figures
+if save_fig:
+	print 'Saving figs'
+	fig_coh.savefig(coh_fig_file)
+	fig_cor.savefig(cor_fig_file)
+   
+   
