105764398395dfe4f3075ff4cdb4c672a617a129
Rachel Denison <rdenison@berkeley.edu>
2013-03-13 13:08:44 -0700

    updated rd_mrAdjustInplane to correct the voxel size in reconstructed MN scans

--- users/Rachel/LGN_Code/rd_mrAdjustInplane_MN20120806.m
+++ users/Rachel/LGN_Code/rd_mrAdjustInplane_MN20120806.m
@@ -4,12 +4,18 @@
 
 load mrSESSION.mat
 
-correctInplaneDim = [160 160];
-correctRes = [1.3 1.3 1.3]; % MN [1.2 1.2 1.2] % JN, SB, RD [1.3 1.3 1.3]
+% correctInplaneDim = [160 160];
+correctRes = [1.2 1.2 1.2]; % MN [1.2 1.2 1.2] % JN, SB, RD [1.3 1.3 1.3]
 
-mrSESSION.inplanes.fullSize = correctInplaneDim;
 mrSESSION.inplanes.voxelSize = correctRes;
-mrSESSION.inplanes.crop(2,:) = correctInplaneDim;
-mrSESSION.inplanes.cropSize = correctInplaneDim;
+% mrSESSION.inplanes.fullSize = correctInplaneDim;
+% mrSESSION.inplanes.crop(2,:) = correctInplaneDim;
+% mrSESSION.inplanes.cropSize = correctInplaneDim;
+
+% this part is used to correct voxel size in reconstructed MN scans
+for iFunc = 1:length(mrSESSION.functionals)
+    mrSESSION.functionals(iFunc).voxelSize = correctRes;
+    mrSESSION.functionals(iFunc).effectiveResolution = correctRes;
+end
 
 save mrSESSION.mat mrSESSION dataTYPES
