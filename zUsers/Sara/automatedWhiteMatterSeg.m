8a3060d0959232bfe8d4ae1eedea9d4fa42f278b
spopham <spopham@berkeley.edu>
2015-10-23 12:16:45 -0700

    Automated White Matter Seg Upload

--- zUsers/Sara/automatedWhiteMatterSeg.m
+++ zUsers/Sara/automatedWhiteMatterSeg.m
@@ -1,4 +1,4 @@
-function automatedWhiteMatterSeg(subjectID,mprageFile)
+function automatedWhiteMatterSeg(subjectID)
 % ------------------------------------------------------------------------------------------------------------
 % fMRI processing pipeline whose goal is to automatize steps as much as
 % possible
@@ -8,27 +8,59 @@ function automatedWhiteMatterSeg(subjectID,mprageFile)
 % Byrne, Summer Sheremata
 % ------------------------------------------------------------------------------------------------------------
 
-answer = input('Have you already aligned high-res and low-res anatomicals? (y/n)   ','s');
+%% FOLDER SETUP AND INITIAL CHECKS
+disp('You should be in the 06_mrVista_session folder for the subject that you are analyzing.');
+answer = input('Is this correct? (y/n)  ','s');
 if strmatch('y',lower(answer))
-    disp('Hooray, you are competent! We will now continue to white matter segmentation...');
+    disp('OK, continuing...');
+    disp(' ');
 elseif strmatch('n',lower(answer))
-    error('Go do that first, you dummy.');
+    error('Please start in the correct path and try again.');
 else
     error('Input not understood.');
 end
 
-% set freesurfer environment in terminal window
-% CANNOT FIGURE OUT HOW TO GET tcsh TO WORK SO AM TRYING ALTERNATIVE
-% Let me know if this is bad, but I don't think it will change anything
+destinationFolder = cd;
+mprageFile = [destinationFolder '/nifti/nu.nii.gz'];
 
-% need a personal pathFile with freesurfer_folder variable
-load('pathFile.mat')
+answer = input('Have you already aligned high-res and low-res anatomicals? (y/n)   ','s');
+if strmatch('y',lower(answer))
+    disp('Hooray, you are competent! We will now continue to white matter segmentation...');
+    disp(' ');
+elseif strmatch('n',lower(answer))
+    error('Please complete that step before continuing with segmentation.');
+else
+    error('Input not understood.');
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% CANNOT FIGURE OUT HOW TO GET tcsh TO WORK SO AM TRYING AN ALTERNATIVE %
+% Let me know if this is bad, but I don't think it will change anything %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% WHITE MATTER SEGMENTATION
+if exist([getenv('SUBJECTS_DIR') '/' subjectID])==7
+    disp(['A subject folder for this participant already exists in ' getenv('SUBJECT_DIR')]);
+    error('Please change subjectID to run white matter segmentation again.');
+end
 
 success = system(['recon-all -i ' mprageFile ' -subjid ' subjectID ' -all']);
 if success~=0
     error('Error in white matter segmenation... See other messages in command window for details.');
 end
 
+cd([getenv('SUBJECTS_DIR') '/' subjectID '/mri'])
 
+%% MGZ TO NII CONVERSION
+success = system(['mgz2nii.sh ' subjectID]);
+if success~=0
+    error('Error in conversion from mgz to nii file type.');
+end
 
+%% SETTING UP FILES FOR ITKGRAY
+fs_ribbon2itk(subjectID,[],[],[getenv('SUBJECTS_DIR') '/' subjectID '/mri/' subjectID '_nu.nii.gz'],[],'RAS');
 
+success = copyfile([getenv('SUBJECTS_DIR') '/' subjectID '/mri/' subjectID '_nu.nii.gz'],destinationFolder);
+if success; disp(['Copying ' subjectID '_nu.nii.gz file... Done']);else error(status); end
+success = copyfile([getenv('SUBJECTS_DIR') '/' subjectID '/mri/t1_class.nii.gz'],destinationFolder);
+if success; disp('Copying t1_class.nii.gz file... Done');else error(status); end
